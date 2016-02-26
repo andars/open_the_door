a9b1dc1ff02931474766d58ea29d1799406aaeb8
Andrew Foote <afoote97@gmail.com>
2016-02-25 20:06:19 -0800

    add close_door command & return 200

--- server.py
+++ server.py
@@ -4,13 +4,18 @@ import SocketServer
 
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial('/dev/ttyUSB1', 9600)
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/open_door':
             ser.write('o')             
-        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+            self.send_response(200)
+        elif self.path == '/close_door':
+            ser.write('c')
+            self.send_response(200)
+        else:
+            return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 handler = MyRequestHandler
 server = SocketServer.TCPServer(('0.0.0.0', 8080), handler)
